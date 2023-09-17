392379fd2b1e7373c32a41c3464864e979e149e1
Alper Guven <alper.guven@tarfin.com>
2023-09-17 10:58:04 +0300

    Initial commit for x-state login screen example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from "./layouts/LoginPage.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="grid-layout">
+    <div class="register-rule-1">1</div>
+    <div class="register-rule-2">2</div>
+    <div class="register-rule-3">3</div>
+    <div class="register-rule-4">4</div>
+    <div class="register-input">5</div>
+    <div class="register-rule-5">6</div>
+    <div class="register-rule-6">7</div>
+    <div class="register-rule-7">8</div>
+    <div class="register-rule-8">9</div>
+  </section>
+  <LoginPage />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
